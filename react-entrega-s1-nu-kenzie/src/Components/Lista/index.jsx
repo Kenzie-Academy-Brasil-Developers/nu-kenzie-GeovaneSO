4d5f18016a12a5d3f7b974f1c7bebb1ca72cf371
Geovane Oliveira <olivira.5geovane@outlook.com>
2022-07-27 08:53:45 -0300

    Nu kenzie atualizado

--- react-entrega-s1-nu-kenzie/src/Components/Lista/index.jsx
+++ react-entrega-s1-nu-kenzie/src/Components/Lista/index.jsx
@@ -2,11 +2,30 @@ import Button from "../Button";
 import './style.css';
 function Card({listTransactions, transaction, index, setListTransactions}){
     return (
-        <li id={index}>
+        transaction.type === 'Entrada' ?
+        <li className="li">
             <div className="container">
                 <div className="box__description">
                     <h3 className="description">{transaction.description}</h3>
-
+                </div>
+                <div className="box__value">
+                    {
+                    transaction.value >= 0 ?
+                        <p>$ {transaction.value }</p> 
+                        :   <p>$ {transaction.value * -1}</p>
+                    } 
+                </div>
+                <div className='box__btn'>
+                    <Button description={transaction.description} setListTransactions={setListTransactions} listTransactions={listTransactions} index={index}/>
+                </div>
+            </div>
+            <div className="box__type">
+                <span className="type">{transaction.type}</span>
+            </div>
+        </li> :  <li className="li2">
+            <div className="container">
+                <div className="box__description">
+                    <h3 className="description">{transaction.description}</h3>
                 </div>
                 <div className="box__value">
                     {
--- react-entrega-s1-nu-kenzie/src/Components/TotalMoney/index.jsx
+++ react-entrega-s1-nu-kenzie/src/Components/TotalMoney/index.jsx
@@ -9,7 +9,7 @@ function TotalMoney({listTransactions}){
     return(
         <div className='box__container--value'>
             <div className="totalMoney">
-                <div className='box__value'>
+                <div className='box__value--money'>
                     <h3>Valor Total:</h3>
                     <p className='value'>$ {totalValue},00</p>
                 </div>
